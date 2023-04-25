662496b5e4d5d0d042046eaf71b603aa380d154a
lazyshivam <shivamgoswami.ss.pp@gmail.com [filter lfs]>
2023-04-25 12:04:26 +0530

    video call integrated

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ const cors = require("cors");
 const { URLSearchParams } = require("url");
 require('dotenv').config();
 
-const app = express();
+const app=express();
 app.use(cors());
 app.use(express.json());
 connectToMongo();
+const httpServer = createServer(app);
+
+// Initialize PeerJS server
 
 app.use('/api/auth',require('./routes/auth'));
 
-const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: ["http://localhost:3000"],
@@ -30,6 +32,7 @@ const users={};
 console.log(Object.keys(users).length);
 /*socket releted stuff*/
 io.on("connection", (socket) => {
+  socket.emit('me', socket.id);
    socket.on("new-user-joined", (user) => {{
          users[user.email] =user;
          console.log("user joined ",users[user.email]);
@@ -37,36 +40,34 @@ io.on("connection", (socket) => {
    }});
   socket.on("send_message", (data) => {
     socket.broadcast.emit("receive_message", data);
-    console.log("Send message: " + data);
   });
   
-  socket.on("disconnect", (reason) => {
-    if (reason === "io server disconnect") {
-      // the disconnection was initiated by the server, you need to reconnect manually
-      socket.connect();
-      
-    }
+
+  // ################for video call################
+   // Join a room
+
+   // Handle call requests
+   socket.on('callUser', ({userToCall,signalData,from,name}) => {
+     io.to(userToCall).emit('calluser', { signal: signalData, from,name });
+   });
+ 
+   // Handle call acceptance
+   socket.on('answercall', (data) => {
+     io.to(data.to).emit('callaccepted', data.signal);
+   });
+
+
+   socket.on("disconnect", () => {
+     socket.broadcast.emit("User disconnected")
     console.log("User disconnected:",socket.id);
     // else the socket will automatically try to reconnect
   });
-
   socket.on('error', (error) => {
     console.error(`Socket error: ${error}`);
   });
-  
-  
 });
 
 httpServer.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-
-// setInterval(()=>{
-  // if (Object.keys(users).length !== 0) {
-    // console.log('The object is empty');
-    console.log(users)
-  // } 
-  // console.log("hi there")
-
-// },5000)
