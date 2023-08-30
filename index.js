49db5f1e4f41c8229d5449a2c3aacca35f11f2ce
lazyshivam <shivamgoswami.ss.pp@gmail.com [filter lfs]>
2023-08-31 03:24:39 +0530

    user joined event implemented

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require("express");
-const connectToMongo=require('./db')
+const connectToMongo = require('./db');
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const { URLSearchParams } = require("url");
+// const { URLSearchParams } = require("url");
 require('dotenv').config();
 
-const app=express();
+const app = express();
 app.use(cors());
 app.use(express.json());
 connectToMongo();
 const httpServer = createServer(app);
 
-// Initialize PeerJS server
 
-app.use('/api/auth',require('./routes/auth'));
+
+app.use('/api/auth', require('./routes/auth'));
 
 const io = new Server(httpServer, {
   cors: {
@@ -28,45 +28,11 @@ app.use("/", (req, res) => {
   res.send("server is running");
 });
 
-const users={};
-console.log(Object.keys(users).length);
-/*socket releted stuff*/
-io.on("connection", (socket) => {
-  socket.emit('me', socket.id);
-   socket.on("new-user-joined", (user) => {{
-         users[user.email] =user;
-         console.log("user joined ",users[user.email]);
-        socket.broadcast.emit("user-joined",users);
-   }});
-  socket.on("send_message", (data) => {
-    socket.broadcast.emit("receive_message", data);
-  });
-  
-
-  // ################for video call################
-   // Join a room
-
-   // Handle call requests
-   socket.on('callUser', ({userToCall,signalData,from,name}) => {
-    console.log("Call received from",userToCall);
-     io.to(userToCall).emit('calluser', { signal: signalData, from,name });
-   });
- 
-   // Handle call acceptance
-   socket.on('answercall', (data) => {
-     io.to(data.to).emit('callaccepted', data.signal);
-   });
-
-
-   socket.on("disconnect", () => {
-     socket.broadcast.emit("User disconnected")
-    console.log("User disconnected:",socket.id);
-    // else the socket will automatically try to reconnect
-  });
-  
-});
+// Import the initializeSocket function from socketFunctions.js
+const { initializeSocket } = require('./socketFunctions');
+// Initialize socket functionality
+initializeSocket(io);
 
 httpServer.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
---
+++ socketFunctions.js
@@ -0,0 +1,54 @@
+const users = {};
+const formatTime = (date) => {
+  return `${date.getHours()}:${date.getMinutes()}`;
+}
+const initializeSocket = (io) => {
+  io.on("connection", (socket) => {
+    socket.emit("me", socket.id);
+
+    socket.on("new-user-joined", (user) => {
+      users[socket.id] = user;
+      console.log("user joined ", users);
+      const messageData = {
+        content: user.name+" "+"joined",
+        time: formatTime(new Date()),
+        sender: {name:user.name, email:user.email},
+        type:'alert'
+    };
+      socket.broadcast.emit("user_joined", messageData);
+    });
+
+    socket.on("send_message", (data) => {
+      socket.broadcast.emit("receive_message", data);
+    });
+
+    // ################for video call################
+    // Handle call requests
+    socket.on('callUser', ({ userToCall, signalData, from, name }) => {
+      console.log("Call received from", userToCall);
+      io.to(userToCall).emit('calluser', { signal: signalData, from, name });
+    });
+
+    // Handle call acceptance
+    socket.on('answercall', (data) => {
+      io.to(data.to).emit('callaccepted', data.signal);
+    });
+
+    socket.on("disconnect", () => {
+      if (users[socket.id]) {
+        const disconnectedUser = users[socket.id];
+        const messageData = {
+          content: disconnectedUser.name+" "+"left",
+          time: formatTime(new Date()),
+          sender: {name:disconnectedUser.name, email:disconnectedUser.email},
+          type:'alert'
+      };
+        delete users[socket.id];
+        socket.broadcast.emit("user-left", messageData);
+        console.log("User disconnected:", socket.id);
+      }
+    });
+  });
+};
+
+module.exports = { initializeSocket };
