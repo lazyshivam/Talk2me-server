3dfea6e2bd74bf912dd714be24ffa5d315974c01
lazyshivam <shivamgoswami.ss.pp@gmail.com [filter lfs]>
2023-09-01 22:49:53 +0530

    mongo url issue fixed

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const mongoURI=`${process.env.Mongo_URI}/VideoChatApp`;
+const mongoURI=`${process.env.Mongo_URI}`;
 
 //connecting with the mangodb database
 const connectToMongo= ()=>{
